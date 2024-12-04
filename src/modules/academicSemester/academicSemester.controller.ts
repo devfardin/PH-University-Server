586d88d4ae2d12315163c8057e0f2d5416e748d3
devfardin <contactfardin22@gmail.com>
2024-12-04 14:46:40 +0600

    create services

--- src/modules/academicSemester/academicSemester.controller.ts
+++ src/modules/academicSemester/academicSemester.controller.ts
@@ -1,13 +1,17 @@
 import catchAsync from '../../app/utils/catchAsync';
 import sendResponse from '../../app/utils/sendResponse';
 import httpStatus from 'http-status';
+import { AcademicSemesterServices } from './academicSemester.service';
 
 const createAcademicSemester = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.createAcademicSemesterIntoDB(
+    req.body,
+  );
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Academic Semester is created successfully',
-    data: '',
+    data: result,
   });
 });
 
--- src/modules/academicSemester/academicSemester.route.ts
+++ src/modules/academicSemester/academicSemester.route.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validationRequest from '../../app/middlewares/validateRequest';
+import { AcademicSemesterValidations } from './acedemicSemester.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-semester',
+  validationRequest(
+    AcademicSemesterValidations.createAcdemicSemesterValidationSchema,
+  ),
   AcademicSemesterController.createAcademicSemester,
 );
 
