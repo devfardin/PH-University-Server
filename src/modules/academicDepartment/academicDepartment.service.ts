52075524e0680cc53effdf5c2ce606a8063725bd
devfardin <contactfardin22@gmail.com>
2024-12-08 14:10:33 +0600

    academic department services

--- src/modules/academicDepartment/academicDepartment.service.ts
+++ src/modules/academicDepartment/academicDepartment.service.ts
@@ -5,3 +5,29 @@ const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
   const result = await AcademicDepartmentModel.create(payload);
   return result;
 };
+const getAllAcademicDepartment = async () => {
+  const result = AcademicDepartmentModel.find();
+  return result;
+};
+const getSingeAcademicDepartment = async (id: string) => {
+  const result = AcademicDepartmentModel.findById(id);
+  return result;
+};
+const updateAcademcDepartment = async (
+  id: string,
+  payload: Partial<TAcademicDepartment>,
+) => {
+  const result = await AcademicDepartmentModel.findByIdAndUpdate(
+    { _id: id },
+    payload,
+    { new: true },
+  );
+  return result;
+};
+
+export const AcademicDepartmentSerives = {
+  createAcademicDepartmentIntoDB,
+  getAllAcademicDepartment,
+  getSingeAcademicDepartment,
+  updateAcademcDepartment,
+};
