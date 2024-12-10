a3eb3bd3c5733cbace3acd2f453b5a97eaa3d754
devfardin <contactfardin22@gmail.com>
2024-12-10 23:35:39 +0600

    make error formate for zod validation

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,21 +1,52 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response, NextFunction } from 'express';
 
-const globalErrorHandler = (
-  error: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || 'Internal Server Error';
+import { ErrorRequestHandler } from 'express';
+import { TErrorSource } from '../interface/error';
+import { ZodError, ZodIssue } from 'zod';
+import config from '../config';
 
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  // Setting default values
+  let statusCode = error.statusCode || 500;
+  let message = error.message || 'Internal Server Error';
+
+  var errorSourses: TErrorSource = [
+    {
+      path: '',
+      message: 'Someting want wrong',
+    },
+  ];
+
+  const handleZodError = (error: ZodError) => {
+    const statusCode = 400;
+    const errorSourses: TErrorSource = error.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue?.message,
+      };
+    });
+    return {
+      statusCode,
+      message: 'validation error',
+      errorSourses,
+    };
+  };
+
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSourses = simplifiedError.errorSourses;
+  }
+
+  // Ultimate return
   res.status(statusCode).json({
     success: false,
     message,
-    error,
+    errorSourses,
+    stack: config.NODE_ENV === 'Development' ? error.stack : null,
+    // error,
   });
 };
 
