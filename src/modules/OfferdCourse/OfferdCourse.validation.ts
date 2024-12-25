e620d48f3e1aea07bdf5d4adff5194aa17e11735
devfardin <contactfardin22@gmail.com>
2024-12-25 17:04:50 +0600

    check start and end time for validation

--- src/modules/OfferdCourse/OfferdCourse.validation.ts
+++ src/modules/OfferdCourse/OfferdCourse.validation.ts
@@ -2,34 +2,45 @@ import { z } from 'zod';
 import { Days } from './OfferdCourse.constant';
 
 const creatreOfferedCourseValidationSchema = z.object({
-  body: z.object({
-    semesterRegistration: z.string(),
-    academicFaculty: z.string(),
-    academicDepartment: z.string(),
-    course: z.string(),
-    faculty: z.string(),
-    section: z.number(),
-    maxCapacity: z.number(),
-    days: z.array(z.enum(Days)),
-    startTime: z.string().refine(
-      (time) => {
-        const timeFormat = /^([01]\d|2[0-3]):([0-5]\d)$/;
-        return timeFormat.test(time);
+  body: z
+    .object({
+      semesterRegistration: z.string(),
+      academicFaculty: z.string(),
+      academicDepartment: z.string(),
+      course: z.string(),
+      faculty: z.string(),
+      section: z.number(),
+      maxCapacity: z.number(),
+      days: z.array(z.enum(Days)),
+      startTime: z.string().refine(
+        (time) => {
+          const timeFormat = /^([01]\d|2[0-3]):([0-5]\d)$/;
+          return timeFormat.test(time);
+        },
+        {
+          message: 'Invalid Time format, Expected "HH:MM" in Hours formate',
+        },
+      ),
+      endTime: z.string().refine(
+        (time) => {
+          const timeFormat = /^([01]\d|2[0-3]):([0-5]\d)$/;
+          return timeFormat.test(time);
+        },
+        {
+          message: 'Invalid Time format, Expected "HH:MM" in Hours formate',
+        },
+      ),
+    })
+    .refine(
+      (body) => {
+        const startTime = new Date(`1970-01-01T${body.startTime}:00`);
+        const endTime = new Date(`1970-01-01T${body.endTime}:00`);
+        return endTime > startTime;
       },
       {
-        message: 'Invalid Time format, Expected "HH:MM" in Hours formate',
+        message: 'Satrt time should be before end time',
       },
     ),
-    endTime: z.string().refine(
-      (time) => {
-        const timeFormat = /^([01]\d|2[0-3]):([0-5]\d)$/;
-        return timeFormat.test(time);
-      },
-      {
-        message: 'Invalid Time format, Expected "HH:MM" in Hours formate',
-      },
-    ),
-  }),
 });
 const updateOfferCourseValidationSchema = z.object({
   body: z.object({
