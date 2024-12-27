e49f470965a9af776bde0b017af4e5a29aa6c621
devfardin <contactfardin22@gmail.com>
2024-12-27 14:16:57 +0600

    for loop

--- src/modules/OfferdCourse/OfferedCourse.utils.ts
+++ src/modules/OfferdCourse/OfferedCourse.utils.ts
@@ -4,15 +4,16 @@ export const hasTimeConflict = (
   assignedSchedules: TSchedule[],
   newSchedule: TSchedule,
 ) => {
-  assignedSchedules.forEach((schedule) => {
+  for (const schedule of assignedSchedules) {
     const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
     const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
     const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
     const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
 
     if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+      console.log('Yes');
       return true;
     }
-  });
+  }
   return false;
 };
