c512be004dd24679d70c98bbedaef8d42c81a09c
devfardin <contactfardin22@gmail.com>
2024-12-31 16:20:21 +0600

    update

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -13,9 +13,9 @@ export const sendEmail = async () => {
 
   await transporter.sendMail({
     from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    to: 'bar@example.com, baz@example.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world?', // plain text body
+    html: '<b>Hello world?</b>', // html body
   });
 };
