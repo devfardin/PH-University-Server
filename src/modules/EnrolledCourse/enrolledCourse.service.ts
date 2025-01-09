a7855a262d03a1a1a191f82d3acffb1e334f0860
devfardin <contactfardin22@gmail.com>
2025-01-09 20:52:12 +0600

    update

--- src/modules/EnrolledCourse/enrolledCourse.service.ts
+++ src/modules/EnrolledCourse/enrolledCourse.service.ts
@@ -1,14 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import AppError from '../../app/errors/AppError';
 import { OfferdCourseModel } from '../OfferdCourse/OfferdCourse.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
 import httpStatus from 'http-status';
 import EnrolledCourseModel from './enrolledCourse.model';
 import { Student } from '../student/student.model';
+import mongoose from 'mongoose';
 const createEnrolledCourseIntoDB = async (
   userId: string,
   payload: TEnrolledCourse,
 ) => {
   const { offeredCourse } = payload;
+
   // check if the offered course not exist
   const isOfferedCourseExist = await OfferdCourseModel.findById(offeredCourse);
   if (!isOfferedCourseExist) {
@@ -20,6 +23,7 @@ const createEnrolledCourseIntoDB = async (
   }
   // check if the student not exist
   const student = await Student.findOne({ id: userId }).select('id');
+
   if (!student) {
     throw new AppError(httpStatus.NOT_FOUND, 'student not found');
   }
@@ -31,6 +35,43 @@ const createEnrolledCourseIntoDB = async (
   });
   if (!isStudentAlreadyEnrolled) {
     throw new AppError(httpStatus.CONFLICT, 'Student already enrolled!');
+  }
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    const reslut = await EnrolledCourseModel.create(
+      [
+        {
+          semesterRegistration: isOfferedCourseExist.semesterRegistration,
+          academicSemester: isOfferedCourseExist.academicSemester,
+          academicFaculty: isOfferedCourseExist.academicFaculty,
+          academicDepartment: isOfferedCourseExist.academicDepartment,
+          offeredCourse: offeredCourse,
+          course: isOfferedCourseExist.course,
+          student: student._id,
+          faculty: isOfferedCourseExist.faculty,
+          isEnrolled: true,
+        },
+      ],
+      { session },
+    );
+    if (!reslut) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        'Failed to enroll in this cours',
+      );
+    }
+    const maxCapacity = isOfferedCourseExist.maxCapacity;
+    await OfferdCourseModel.findByIdAndUpdate(offeredCourse, {
+      maxCapacity: maxCapacity - 1,
+    });
+    await session.commitTransaction();
+    await session.endSession();
+    return reslut;
+  } catch (error: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(error);
   }
 };
 
